4b601e00e2893cb3988381568bd68216dc726498
jotirai <jotirai@live.co.uk>
2014-08-19 14:22:24 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -6,4 +6,22 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-move(18)
+def  go():
+  moved=0
+  while touch() == "fruit":
+    move()
+    moved = moved+1
+    
+    turn (-1)
+    if touch() == "fruit":
+      go()
+      turn(2)
+      if touch() == "fruit":
+      go()
+      
+      turn(1)
+      for i in range (0,moved):
+        move()
+        turn (2)
+        
+go()
